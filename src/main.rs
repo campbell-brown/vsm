d62b3bb6289694594862be077739d05c64429dff
Campbell Brown <campbell.brown@bduncanltd.com>
2025-10-24 13:29:49 +1300

    Initial rust project

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use inquire::Select;
+
+fn main() {
+    let projects = vec!["Project A", "Project B", "Project C"];
+
+    loop {
+        let project = Select::new("Select a project:", projects.clone())
+            .prompt();
+
+        let project = match project {
+            Ok(p) => p,
+            Err(_) => break, // Ctrl+C exits
+        };
+
+        let subprojects = match project.as_ref() {
+            "Project A" => vec!["Sub A1", "Sub A2", "<- Back"],
+            "Project B" => vec!["Sub B1", "Sub B2", "<- Back"],
+            "Project C" => vec!["Sub C1", "<- Back"],
+            _ => vec![],
+        };
+
+        let subproject = Select::new("Select a subproject:", subprojects)
+            .prompt()
+            .unwrap();
+
+        if subproject == "<- Back" {
+            continue; // go back to project selection
+        }
+
+        println!("You selected {} -> {}", project, subproject);
+        break;
+    }
+}
